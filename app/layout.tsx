28e4febf07654275d837ae8638578108a4150ba1
MerelyLogical <qieerbushe7@gmail.com>
2025-09-23 22:25:12 +0100

    smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata =  { title: "Restaurant" }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className="flex min-h-screen flex-col overflow-x-hidden">
         <Navbar />
         <main className="bg-brand/30 flex-1">{children}</main>

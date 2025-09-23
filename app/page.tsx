88a71bf467200b408f00bc9aa0adc515faf92f20
MerelyLogical <qieerbushe7@gmail.com>
2025-09-23 22:27:15 +0100

    change pics in carousel around

--- app/page.tsx
+++ app/page.tsx
@@ -187,8 +187,8 @@ export default function Home() {
         {/* Image Carousel */}
         <PeekCarousel
           images={[
-            { src: "/laziji.jpg", alt: "Spicy beef noodles" },
-            { src: "/chili.PNG", alt: "Pickled chili fish" },
+            { src: "/logo.jpg", alt: "Spicy beef noodles" },
+            { src: "/guizhouren.jpg", alt: "Pickled chili fish" },
             { src: "/running potato.PNG", alt: "Street-side skewers" },
             { src: "/slogan.jpg", alt: "Tofu with chilis" },
             { src: "/menu1.PNG", alt: "Guizhou peppers" },
